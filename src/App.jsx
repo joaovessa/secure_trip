23fe22a10b704695bb2fba1cc4f832ed340ecc3a
João Vitor Silva <jvsa.110507@gmail.com>
2024-08-13 14:19:26 -0300

    vai krai

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Logo from '../icon.png'
+import viteLogo from './assets/bg/home_bg.png'
 import './App.css'
 
 function App() {
